26ee70b791beb6f5fbeea23a0b5ee8822397830f
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2023-03-01 12:27:21 +0100

    Console Logging

--- SecretManagement.ExtensionTemplate/internal/template/SecretManagement.þnameþ/SecretManagement.þnameþ/SecretManagement.þnameþ.Extension/internal/configurations/configuration.ps1
+++ SecretManagement.ExtensionTemplate/internal/template/SecretManagement.þnameþ/SecretManagement.þnameþ/SecretManagement.þnameþ.Extension/internal/configurations/configuration.ps1
@@ -13,3 +13,12 @@ Set-PSFConfig -Module 'þnameþ' -Name 'Example.Setting' -Value 10 -Initialize -
 
 Set-PSFConfig -Module 'þnameþ' -Name 'Import.DoDotSource' -Value $false -Initialize -Validation 'bool' -Description "Whether the module files should be dotsourced on import. By default, the files of this module are read as string value and invoked, which is faster but worse on debugging."
 Set-PSFConfig -Module 'þnameþ' -Name 'Import.IndividualFiles' -Value $false -Initialize -Validation 'bool' -Description "Whether the module files should be imported individually. During the module build, all module code is compiled into few files, which are imported instead by default. Loading the compiled versions is faster, using the individual files is easier for debugging and testing out adjustments."
+
+Set-PSFConfig -Module 'SecretManagement.þnameþ.Extension' -Name 'ConsoleLogging.enabled' -Value $true -Initialize -Validation 'bool' -Description "If set to `$true, Messages from the module will be displayed in the main console"
+# Set-PSFConfig -Module 'SecretManagement.þnameþ.Extension' -Name 'ConsoleLogging.style' -Value 'CON: %Message%' -Initialize -Validation string -Description "What should be displayed in the console?"
+# Set-PSFConfig -Module 'SecretManagement.þnameþ.Extension' -Name 'ConsoleLogging.style' -Value '%Tags%|%Level%|%Module%|%FunctionName%: %Message%' -Initialize -Validation string -Description "What should be displayed in the console?"
+Set-PSFConfig -Module 'SecretManagement.þnameþ.Extension' -Name 'ConsoleLogging.style' -Value '[%Level%] %Message%' -Initialize -Validation string -Description "What should be displayed in the console?"
+# Possible levels: Critical (1), Important / Output / Host (2), Significant (3), VeryVerbose (4), Verbose (5), SomewhatVerbose (6), System (7), Debug (8), InternalComment (9), Warning
+#         (666) Either one of the strings or its respective number will do as input.
+Set-PSFConfig -Module 'SecretManagement.þnameþ.Extension' -Name 'ConsoleLogging.MinLevel' -Value 1 -Initialize -Validation integer -Description "From which level starting on should messages be sent to the provider?"
+Set-PSFConfig -Module 'SecretManagement.þnameþ.Extension' -Name 'ConsoleLogging.MaxLevel' -Value 1 -Initialize -Validation integer -Description "Up to which level should messages be sent to the provider?"
---
+++ SecretManagement.ExtensionTemplate/internal/template/SecretManagement.þnameþ/SecretManagement.þnameþ/SecretManagement.þnameþ.Extension/internal/scripts/console_logging.ps1
@@ -0,0 +1,17 @@
+﻿# TODO Alternative logging to be created
+Write-PSFMessage "Configure Console Logging"
+if (Get-PSFConfigValue -FullName 'SecretManagement.þnameþ.Extension.ConsoleLogging.enabled' -Fallback $false) {
+    $providerParam=@{
+        Name="console"
+        Enabled=$true
+        style=Get-PSFConfigValue -FullName 'SecretManagement.þnameþ.Extension.ConsoleLogging.style'
+        MinLevel=Get-PSFConfigValue -FullName 'SecretManagement.þnameþ.Extension.ConsoleLogging.MinLevel'
+        MaxLevel=Get-PSFConfigValue -FullName 'SecretManagement.þnameþ.Extension.ConsoleLogging.MaxLevel'
+        InstanceName = 'SecretManagement.þnameþ.console'
+    }
+    Write-PSFMessage "Configure Console Logging with Param=$($providerParam|ConvertTo-Json -Compress)"
+    Set-PSFLoggingProvider @providerParam -IncludeModules 'SecretManagement.þnameþ.Extension'
+
+}else{
+    Write-PSFMessage "NOT Configure Console Logging"
+}

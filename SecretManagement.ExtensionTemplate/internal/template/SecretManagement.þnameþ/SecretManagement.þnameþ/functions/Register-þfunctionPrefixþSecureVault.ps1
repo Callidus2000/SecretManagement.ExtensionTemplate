331e6a27faeafcf7ba0cfcdaa07554ee0a2361e1
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2023-03-01 10:57:06 +0100

    adaptive module path

--- SecretManagement.ExtensionTemplate/internal/template/SecretManagement.þnameþ/SecretManagement.þnameþ/functions/Register-þfunctionPrefixþSecureVault.ps1
+++ SecretManagement.ExtensionTemplate/internal/template/SecretManagement.þnameþ/SecretManagement.þnameþ/functions/Register-þfunctionPrefixþSecureVault.ps1
@@ -1,63 +1,12 @@
 ﻿function Register-þfunctionPrefixþSecureVault {
     <#
     .SYNOPSIS
-    Registers a new Secretvault.
+    Registers a new Secretvault of type þnameþ.
 
     .DESCRIPTION
-    Registers a new Secretvault.
+    Registers a new Secretvault of type þnameþ.
 
-    .PARAMETER VaultName
-    The name of the vault.
-
-    .PARAMETER Server
-    The hostname of the server which provides the service.
-
-    .PARAMETER Port
-    On which port is the server listening? Defaults to 11016.
-
-    .PARAMETER Database
-    The name of the Database.
-
-    .PARAMETER UserName
-    The username which is used to connect to the server. Has to be specified on vault level as Unlock-SecretVault only accepts a password instead credentials.
-
-    .PARAMETER defaultOUName
-    Under which OU should new entries be created by default?
-    If not set the first entry of all active OUs, sorted by Type (Group before User) and Name will be used
-
-    .PARAMETER defaultFormName
-    Which password form should new entries be created by default?
-    If not set the first entry of all active forms, sorted by field counts and name lenth will be used.
-    This should be the less complex form of all ;-)
-
-    .PARAMETER FormMapping
-    The manual configured form mapping.
-
-    .PARAMETER InterActive
-    If this switch is used the user can interactively choose (via Out-GridView) an existing config from the installed Windows Client.
-
-    .PARAMETER Confirm
-    Not respected
-
-    .PARAMETER WhatIf
-    If used, the vault will not be registered but the register command will be printed
-
-    .EXAMPLE
-    Register-NetwrixSecureVault -VaultName myVault -Server myserver -Database PWDB -UserName fred
-
-    Registers the given vault.
-    .EXAMPLE
-    Register-NetwrixSecureVault -VaultName myVault -Interactive
-
-    Shows all preconfigured connections and stores it with the name myVault.
-    .EXAMPLE
-    Register-NetwrixSecureVault -Interactive
-
-    Shows all preconfigured connections and stores it with the name of the database connection.
-    .EXAMPLE
-    Register-NetwrixSecureVault -Interactive -WhatIf
-
-    Shows all preconfigured connections and show the commands necessary to configure it manually.
+    #TODO Add parameter description
 
     .NOTES
     General notes
@@ -66,102 +15,32 @@
     param (
 
         [parameter(mandatory = $true, ParameterSetName = "cmdLine")]
-        [parameter(mandatory = $true, ParameterSetName = "interactiveWithRename")]
-        [string]$VaultName,
-        [parameter(mandatory = $true, ParameterSetName = "cmdLine")]
-        [string]$Server,
-        [string]$Port = "11016",
-        [parameter(mandatory = $true, ParameterSetName = "cmdLine")]
-        [string]$Database,
-        [parameter(mandatory = $true, ParameterSetName = "cmdLine")]
-        [string]$UserName,
-        [parameter(mandatory = $false, ParameterSetName = "cmdLine")]
-        [string]$defaultOUName,
-        [parameter(mandatory = $false, ParameterSetName = "cmdLine")]
-        [string]$defaultFormName,
-        [parameter(mandatory = $false, ParameterSetName = "cmdLine")]
-        $FormMapping,
-        [parameter(mandatory = $true, ParameterSetName = "interactive")]
-        [parameter(mandatory = $true, ParameterSetName = "interactiveWithRename")]
-        [switch]$InterActive
+        [string]$VaultName
+        # TODO add your specific parameters
     )
-
-    $myModulePath = "$ModuleRoot\SecretManagement.NetwrixPasswordSecure.psd1"
+    # In order to register a vault you need the module name. For dev purposes
+    # this code (see configuration.ps1) enables the detection if the module is installed
+    # or the PSD1 file is imported directly.
+    $myModulePath = "$ModuleRoot\SecretManagement.þnameþ.psd1"
     $psdData = Import-PowerShellDataFile $myModulePath
     $registerParam = @{
-        ModuleName = Get-PSFConfigValue 'SecretManagement.NetwrixPasswordSecure.InterActiveRegister.modulepath'
-    }
-    if ($PSCmdlet.ParameterSetName -eq 'interactive') {
-        $registryLocations = Get-PSFConfigValue -FullName 'SecretManagement.NetwrixPasswordSecure.InterActiveRegister.registy'
-        $possibleVaults = @()
-        foreach ($regLoc in $registryLocations) {
-            $regVaultNames = Get-ChildItem $regLoc -ErrorAction SilentlyContinue | Select-Object -ExpandProperty PSChildName
-
-            foreach ($regVaultName in $regVaultNames) {
-                $properties = Get-ItemProperty "$regLoc\$regVaultName"
-                # $settingsHash.vaultName=$vaultName
-                $possibleVaults += [PSCustomObject][ordered]@{
-                    VaultName = $regVaultName
-                    Server    = $properties.HostIP
-                    DataBase  = $properties.DataBaseName
-                }
-            }
-        }
-        $fileConfig = Get-PSFConfigValue -FullName 'SecretManagement.NetwrixPasswordSecure.InterActiveRegister.file'
-        if (Test-Path $fileConfig) {
-            $localConfig = Get-Content $fileConfig | ConvertFrom-Json | Select-Object -ExpandProperty DatabaseProfiles
-            foreach ($conf in $localConfig) {
-                $possibleVaults += [PSCustomObject][ordered]@{
-                    VaultName = $conf.ProfileName
-                    Server    = $conf.HostIP
-                    DataBase  = $conf.DataBaseName
-                }
-
-            }
-        }
-        $newVaultData = $possibleVaults | Sort-Object -Unique -Property VaultName | Out-GridView -OutputMode Single -Title "Choose which account should be added as a Secret Management Vault"
-        if ($null -eq $newVaultData) {
-            Write-PSFMessage -Level Warning "No data chosen, exit."
-            return
-        }
-        Write-PSFMessage "Using chosen data: $($newVaultData|ConvertTo-Json -Compress)"
-        if (-not [string]::IsNullOrWhiteSpace($VaultName)) { $newVaultData.vaultName=$VaultName}
-        $userName = Get-NetwrixUserInput -Title "Enter username for the new vault $($newVaultData.vaultName)" -Default $env:USERNAME
-        if ([string]::IsNullOrWhiteSpace($userName)) {
-            Write-PSFMessage -Level Warning "No username entered, cannot configure vault."
-            return
-        }
-        $registerParam.Name = $newVaultData.VaultName
-        $additionalParameter = @{
-            'Server'   = $newVaultData.Server
-            'Port'     = $Port
-            'Database' = $newVaultData.Database
-            'UserName' = $UserName
-        }
-    }
-    else {
-        $registerParam.Name =$VaultName
-        $additionalParameter = $PSBoundParameters | ConvertTo-PSFHashtable -Inherit -Include @(
-            'Server'
-            'Port'
-            'Database'
-            'UserName'
-            'defaultOUName'
-            'defaultFormName'
-            'FormMapping'
+        ModuleName      = Get-PSFConfigValue 'SecretManagement.þnameþ.InterActiveRegister.modulepath'
+        Name            = $VaultName
+        VaultParameters = $PSBoundParameters | ConvertTo-PSFHashtable -Inherit -Exclude @(
+            # TODO Include all function parameter names which should be transferred to the Vault-Parameters
+            # 'Server'
+            # 'Port'
+            # 'Database'
+            # 'UserName'
         )
-        $additionalParameter.Port = $Port
     }
-    $registerParam.VaultParameters = $additionalParameter
-    $additionalParameter.version = $psdData.ModuleVersion
+    $registerParam.VaultParameters.version = $psdData.ModuleVersion
     if ($PSCmdlet.ShouldProcess($name)) {
         Write-PSFMessage "Registering Vault $vault with Param $($registerParam|ConvertTo-Json -Compress)"
-        # Write-PSFMessage "Registering Vault $vault with Param $($additionalParameter|ConvertTo-Json -Compress) and Module $myModulePath, Version $($psdData.ModuleVersion)"
         Register-SecretVault @registerParam
-        # Register-SecretVault -Name $vaultName -ModuleName $myModulePath -VaultParameters $additionalParameter
     }
     else {
         Write-PSFMessage -Level Host "WhatIf used, no vault registered."
-        Write-PSFMessage -Level Host "You may register the chosen vault later with the following command:`r`n@registerParam=$($registerParam|ConvertTo-Metadata -AsHashtable)`r`nRegister-SecretVault @registerParam"
+        Write-PSFMessage -Level Host "You may register the chosen vault later with the following command:`r`n@registerParam=$($registerParam|ConvertTo-Metadata -AsHashtable)`r`nRegister-þfunctionPrefixþSecureVault @registerParam"
     }
 }
--- SecretManagement.ExtensionTemplate/internal/template/SecretManagement.þnameþ/SecretManagement.þnameþ/internal/configurations/configuration.ps1
+++ SecretManagement.ExtensionTemplate/internal/template/SecretManagement.þnameþ/SecretManagement.þnameþ/internal/configurations/configuration.ps1
@@ -13,3 +13,13 @@ Set-PSFConfig -Module 'þnameþ' -Name 'Example.Setting' -Value 10 -Initialize -
 
 Set-PSFConfig -Module 'þnameþ' -Name 'Import.DoDotSource' -Value $false -Initialize -Validation 'bool' -Description "Whether the module files should be dotsourced on import. By default, the files of this module are read as string value and invoked, which is faster but worse on debugging."
 Set-PSFConfig -Module 'þnameþ' -Name 'Import.IndividualFiles' -Value $false -Initialize -Validation 'bool' -Description "Whether the module files should be imported individually. During the module build, all module code is compiled into few files, which are imported instead by default. Loading the compiled versions is faster, using the individual files is easier for debugging and testing out adjustments."
+
+$currentPath = "$ModuleRoot\SecretManagement.þnameþ.psd1"
+$latestInstalledModules = get-module SecretManagement.þnameþ -ListAvailable | Sort-Object version -Descending | Select-Object -ExpandProperty path -first 1
+Write-PSFMessage "Checking if current module '$currentPath' against latest installed version '$latestInstalledModules'"
+if ($currentPath -eq $latestInstalledModules) {
+    Set-PSFConfig -Module 'SecretManagement.þnameþ' -Name 'InterActiveRegister.modulepath' -Value "SecretManagement.þnameþ" -Initialize -Validation string -Description "Module Path for Vault Registration, Name of the module as latest installed version"
+}
+else {
+    Set-PSFConfig -Module 'SecretManagement.þnameþ' -Name 'InterActiveRegister.modulepath' -Value $currentPath -Initialize -Validation string -Description "Module Path for Vault Registration, development point to local non installed PSM1"
+}
